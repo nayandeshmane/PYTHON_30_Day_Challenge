a4de5010ba549fc25d1c7c9f330b494b540e449b
nayandeshmane <deshmanenayan05@gmail.com>
2025-01-11 23:54:56 +0530

    twentyfive commit

---
+++ python 30 day/Day25_Count Words in a String.py
@@ -0,0 +1,11 @@
+# Count Words in a String
+# Count the number of words in a string.
+
+
+text = "Hello, this is a sample string."
+words = text.split()
+
+# Count the number of words
+word_count = len(words)
+# Display the result
+print(f"Number of words: {word_count}")
