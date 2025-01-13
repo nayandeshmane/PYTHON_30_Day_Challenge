6fe9b1de7dd4c9310c3271dca92b194b0fb6ab65
nayandeshmane <deshmanenayan05@gmail.com>
2025-01-13 18:41:17 +0530

    twentyseven commit

---
+++ python 30 day/Day27_Pattern_Printing.py
@@ -0,0 +1,10 @@
+#Pattern Printing
+#Write a Python program to print the following pattern:
+    
+# *  
+# **  
+# ***  
+# ****  
+# *****
+for i in range(1, 6):  # Loop from 1 to 5 (since there are 5 lines)
+    print('*' * i)  
